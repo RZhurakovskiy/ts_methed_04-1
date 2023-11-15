3555040933849f5830013e822905446348a61f88
RZhurakovskiy <130534672+RZhurakovskiy@users.noreply.github.com>
2023-11-15 13:57:10 +0300

    Add files via upload

---
+++ TS-Methed-05.ts
@@ -0,0 +1,70 @@
+class Job {
+    private role: string;
+    private salary: number;
+
+    constructor(role: string, salary: number) {
+        this.role = role;
+        this.salary = salary;
+    }
+
+    getSalary(): number {
+        return this.salary;
+    }
+
+    work(personName: string): void {
+        console.log(`${personName} сейчас работает как ${this.role}`);
+    }
+}
+
+class Person {
+    private job: Job | undefined;
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+        this.job = undefined;
+    }
+
+    setJob(job: Job): void {
+        this.job = job;
+    }
+
+    getSalary(): number {
+        if (this.job) {
+            return this.job.getSalary();
+        }
+        return 0; // Или можно вернуть другое значение по умолчанию
+    }
+
+    work(): void {
+        if (this.job) {
+            this.job.work(this.name);
+        } else {
+            console.log(`${this.name} не имеет назначенной работы`);
+        }
+    }
+}
+
+const job1 = new Job("Тянуть", 500);
+const job2 = new Job("Толкать", 600);
+const job3 = new Job("Копать", 700);
+
+const person1 = new Person("Иван");
+const person2 = new Person("Петр");
+const person3 = new Person("Анна");
+
+person1.setJob(job1);
+person2.setJob(job2);
+person3.setJob(job3);
+
+person1.work();
+person2.work();
+person3.work();
+
+person1.setJob(job3);
+person2.setJob(job1);
+person3.setJob(job2);
+
+person1.work();
+person2.work();
+person3.work();
